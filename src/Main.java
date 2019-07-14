8a5c658e2e8e1576980ed9e2a9730aa95640dad5
sourov <sourovsarker005@gmail.com>
2019-07-14 12:12:47 +0600

    show all Student task complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,50 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("1. Show all student ");
+        System.out.println("2. Show random student");
+        System.out.println("3. Show number of students");
+        System.out.println("4. Add a student");
+        System.out.println("5. Find a student");
+        System.out.println("Enter Option (1-5):");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int Option = scanner.nextInt();
+
+        if (Option == 1 ){
+            File file = new File("C:\\Users\\TEC\\Desktop\\JavaStudent_list\\src\\Student.txt");
+            BufferedReader bufferedReader = null;
+            try {
+                bufferedReader = new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+            String st;
+            while ((st = bufferedReader.readLine())!= null){
+                System.out.println(st);
+
+            }
+        }
+        else if(Option == 2){
+
+        }
+        else  if(Option == 3){
+
+        }
+        else if(Option == 4){
+
+        }
+        else if(Option == 5){
+
+        }
+        else {
+            System.out.println("Select Correct Option");
+        }
     }
 }
